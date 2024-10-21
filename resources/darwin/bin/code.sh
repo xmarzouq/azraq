33f62ab15ea282abf1ecd2da4719badeb711a4d3
Mohamed Ali <69867660+xmarzouq@users.noreply.github.com>
2024-10-22 02:07:08 +0300

    Update package.json files & test & monaco directories

--- resources/darwin/bin/code.sh
+++ resources/darwin/bin/code.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
-#
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License. See License.txt in the project root for license information.
 
 # when run in remote terminal, use the remote cli
 if [ -n "$VSCODE_IPC_HOOK_CLI" ]; then
